713b4cf61436f550dfa09d5bd1028e50bd696490
Chase Zhang <yun.er.run@gmail.com>
2015-03-30 20:18:59 +0800

    add static files

--- tchelper/settings.py
+++ tchelper/settings.py
@@ -83,6 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Email settings
 EMAIL_BASE_URL = os.environ.get('EMAIL_BASE_URL', '')
